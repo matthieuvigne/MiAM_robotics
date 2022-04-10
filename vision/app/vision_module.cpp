c9d230a6604537f3aefbe61e0eb77db8c0f291ad
Rodolphe <rodolphe.dubois@onera.fr>
2022-04-10 14:47:08 +0200

    [vision] Camera calibration update

--- vision/app/vision_module.cpp
+++ vision/app/vision_module.cpp
@@ -13,12 +13,17 @@ int main(int argc, char* argv[])
   camera_params.name = "camera";
   camera_params.resolution[camera::CameraParams::WIDTH]  = 1280;
   camera_params.resolution[camera::CameraParams::HEIGHT] = 960;
-  camera_params.intrinsics[camera::CameraParams::FX] = 450;
-  camera_params.intrinsics[camera::CameraParams::FY] = 450;
+  camera_params.intrinsics[camera::CameraParams::FX] = 7.0424945444991499e+02;
+  camera_params.intrinsics[camera::CameraParams::FY] = 6.9896472232651024e+02;
   camera_params.intrinsics[camera::CameraParams::CX] = camera_params.resolution[camera::CameraParams::WIDTH] / 2;
   camera_params.intrinsics[camera::CameraParams::CY] = camera_params.resolution[camera::CameraParams::HEIGHT] / 2;
-  camera_params.distortion_model = camera::DistortionModel::Type::NoDistortion;
-  camera_params.distortion_coeffs = {-0.28340811, 0.07395907, 0.00019359, 1.76187114e-05, 0.0};
+  camera_params.distortion_model = camera::DistortionModel::Type::RadTan;
+  camera_params.distortion_coeffs = {
+     2.5432953861499258e-01,
+    -5.8481797342726261e-01,
+    -9.7755009212097837e-04,
+    -2.9062089311574019e-02,
+     3.1200641998405720e-01};
   camera_params.pose = Eigen::Affine3d::Identity();
 
   // Set the module parameters
