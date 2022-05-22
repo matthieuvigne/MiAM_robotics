1f4e5024b1d050a5393c96bb433a48c39be7b712
Rodolphe <rodolphe.dubois@onera.fr>
2022-05-22 17:21:40 +0200

    [vision] Modified the include of filesystem to handle multiple GNU versions

--- vision/app/vision_module.cpp
+++ vision/app/vision_module.cpp
@@ -1,32 +1,47 @@
 #include <iostream>
 
+// Include the filesystem library
+#ifndef __has_include
+  static_assert(false, "__has_include not supported");
+#else
+#  if __cplusplus >= 201703L && __has_include(<filesystem>)
+#    include <filesystem>
+     namespace fs = fs;
+#  elif __has_include(<experimental/filesystem>)
+#    include <experimental/filesystem>
+     namespace fs = std::experimental::filesystem;
+#  elif __has_include(<boost/filesystem.hpp>)
+#    include <boost/filesystem.hpp>
+     namespace fs = boost::filesystem;
+#  endif
+#endif
+
 #include <common/logger.hpp>
 #include <module/module.hpp>
 #ifdef USE_TEST_BENCH
 #include <testing/test_bench.hpp>
 #endif
 
-#include <filesystem>
-
 // Main routine
 int main(int argc, char* argv[])
 {
   // Create new directory for this run
   std::string const rootDir = "/home/pi/vision_module_log";
-  std::filesystem::space_info tmp = std::filesystem::space(rootDir);
+  fs::space_info tmp = fs::space(rootDir);
+  
   long int MIN_DISK_SPACE = 1e9;
   if (tmp.free < MIN_DISK_SPACE)
   {
     std::cout << "Disk almost full - removing old logs" << std::endl;
-    std::filesystem::remove_all(rootDir);
-    std::filesystem::create_directory(rootDir);
+    fs::remove_all(rootDir);
+    fs::create_directory(rootDir);
   }
 
   int dirId = 0;
-  while (std::filesystem::exists((rootDir + "/log" + std::to_string(dirId))))
+  while (fs::exists((rootDir + "/log" + std::to_string(dirId))))
     dirId ++;
   std::string const dirPath = rootDir + "/log" + std::to_string(dirId);
-  std::filesystem::create_directory(dirPath);
+  fs::create_directory(dirPath);
 
   // Initialize the logger
   common::ConsoleLogger::init();
