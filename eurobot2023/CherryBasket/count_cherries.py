c6739cccadd47c60c43ff5133f9a51977413edf4
14chanwa <quentin.cwn@gmail.com>
2023-04-30 17:55:22 +0100

    Rework cherry count and add filtering

--- eurobot2023/CherryBasket/count_cherries.py
+++ eurobot2023/CherryBasket/count_cherries.py
@@ -13,11 +13,7 @@ from fractions import Fraction
 import socket
 import select
 
-from picamera import mmal, mmalobj, exc
-from picamera.mmalobj import to_rational
-
-MMAL_PARAMETER_ANALOG_GAIN = mmal.MMAL_PARAMETER_GROUP_CAMERA + 0x59
-MMAL_PARAMETER_DIGITAL_GAIN = mmal.MMAL_PARAMETER_GROUP_CAMERA + 0x5A
+from set_gain import *
 
 RESOLUTION_X = 800
 RESOLUTION_Y = 608
@@ -25,34 +21,6 @@ RESOLUTION_CHANNELS = 3
 
 RESOLUTION_PARAMETER = (RESOLUTION_X, RESOLUTION_Y)
 RESOLUTION_PARAMETER_WITH_CHANNELS = (RESOLUTION_Y, RESOLUTION_X, RESOLUTION_CHANNELS)
-
-
-def set_gain(camera, gain, value):
-    """Set the analog gain of a PiCamera.
-    
-    camera: the picamera.PiCamera() instance you are configuring
-    gain: either MMAL_PARAMETER_ANALOG_GAIN or MMAL_PARAMETER_DIGITAL_GAIN
-    value: a numeric value that can be converted to a rational number.
-    """
-    if gain not in [MMAL_PARAMETER_ANALOG_GAIN, MMAL_PARAMETER_DIGITAL_GAIN]:
-        raise ValueError("The gain parameter was not valid")
-    ret = mmal.mmal_port_parameter_set_rational(camera._camera.control._port, 
-                                                    gain,
-                                                    to_rational(value))
-    if ret == 4:
-        raise exc.PiCameraMMALError(ret, "Are you running the latest version of the userland libraries? Gain setting was introduced in late 2017.")
-    elif ret != 0:
-        raise exc.PiCameraMMALError(ret)
-
-def set_analog_gain(camera, value):
-    """Set the gain of a PiCamera object to a given value."""
-    set_gain(camera, MMAL_PARAMETER_ANALOG_GAIN, value)
-
-def set_digital_gain(camera, value):
-    """Set the digital gain of a PiCamera object to a given value."""
-    set_gain(camera, MMAL_PARAMETER_DIGITAL_GAIN, value)
-
-
 
 import board
 import adafruit_character_lcd.character_lcd_rgb_i2c as character_lcd
@@ -80,62 +48,40 @@ os.system("rm grey/*.jpg")
 os.system("rm detected/*.jpg")
 os.system("rm filtered/*.jpg")
 
-def count_cherries(img,iter_idx):
 
-  # Threshold the red channel
-  # img = cv2.blur(img, (15,15));
-  # cv2.imwrite("blurred/image{}_blurred.jpg".format(iter_idx), img);
+def local_minimum(img, x, y):
+  # localmin = 255
+  # for i in np.arange(max(0, x - 5), min(img.shape[0]-1, x + 5), 1):
+  #   for j in np.arange(max(0, y - 5), min(img.shape[1]-1, y + 5), 1):
+  #     # print("img[i, j]) ", img[i, j])
+  #     localmin = min(localmin, img[i, j])
+  
+  sliced_img = img[
+    max(0, x - 5):min(img.shape[0]-1, x + 5), 
+    max(0, y - 5):min(img.shape[1]-1, y + 5)]
+  
+  # print(sliced_img.shape)
+
+  localmin = np.quantile(sliced_img, 0.3)
+
+  # print("Local minimum: ", localmin)
+  return localmin
+
+def count_cherries(img, iter_idx):
+
+  # Threshold the channels
   mask_red = cv2.inRange(img[:,:,2], 110, 255);
   mask_green = cv2.inRange(img[:,:,1],0,90);
   mask_blue = cv2.inRange(img[:,:,0],0,90);
   mask = mask_red*mask_green*mask_blue;
   img = cv2.bitwise_and(img,img, mask=mask);
   cv2.imwrite("masked/image{}_masked.jpg".format(iter_idx), img);
-
-  # greyscale
-  # img = img[:, :, 2] #cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  
+  # Convert to grey levels
   gimg = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-  cv2.imwrite("grey/image{}_grey.jpg".format(iter_idx), gimg);
+  cv2.imwrite("grey/image{}_grey.jpg".format(iter_idx), gimg)
 
-  # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(8,8))
-  # # gimg = cv2.erode(gimg,kernel,iterations = 1)
-  # gimg = cv2.morphologyEx(gimg, cv2.MORPH_OPEN, kernel)
-  # gimg = cv2.morphologyEx(gimg, cv2.MORPH_CLOSE, kernel)
-  # # gimg = cv2.Canny(gimg,100,200)
-
-  cv2.imwrite("filtered/image{}_filtered.jpg".format(iter_idx), gimg);
-
-  # Detect the blobs and count them
-  # img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-  # retval, _, stats, _	=	cv2.connectedComponentsWithStatsWithAlgorithm( \
-  #   img, 8, cv2. CV_32S, cv2.CCL_WU);
-
-
-
-  # retval, _, stats, _	=	cv2.connectedComponentsWithStatsWithAlgorithm( \
-  #   img, 8, cv2. CV_32S, cv2.CCL_WU);
-  # num_cherries = 0;
-  # for stat in stats[1:]:
-  #   width = stat[2];
-  #   height = stat[3];
-  #   ok = np.abs(np.log2(width/height))<1.0;
-  #   ok &= min(width,height)>30;
-  #   if ok:
-  #     num_cherries += 1;
-
-  # Apply Hough transform on the blurred image (40, 30, 55) pour ligne cerise frontale
-  # detected_circles=cv2.HoughCircles(gimg,cv2.HOUGH_GRADIENT,1,25,param1=50,param2 =30,minRadius=10,maxRadius=30)
-
-
-
-  # method	Detection method, see HoughModes. The available methods are HOUGH_GRADIENT and HOUGH_GRADIENT_ALT.
-  # dp	Inverse ratio of the accumulator resolution to the image resolution. For example, if dp=1 , the accumulator has the same resolution as the input image. If dp=2 , the accumulator has half as big width and height. For HOUGH_GRADIENT_ALT the recommended value is dp=1.5, unless some small very circles need to be detected.
-  # minDist	Minimum distance between the centers of the detected circles. If the parameter is too small, multiple neighbor circles may be falsely detected in addition to a true one. If it is too large, some circles may be missed.
-  # param1	First method-specific parameter. In case of HOUGH_GRADIENT and HOUGH_GRADIENT_ALT, it is the higher threshold of the two passed to the Canny edge detector (the lower one is twice smaller). Note that HOUGH_GRADIENT_ALT uses Scharr algorithm to compute image derivatives, so the threshold value shough normally be higher, such as 300 or normally exposed and contrasty images.
-  # param2	Second method-specific parameter. In case of HOUGH_GRADIENT, it is the accumulator threshold for the circle centers at the detection stage. The smaller it is, the more false circles may be detected. Circles, corresponding to the larger accumulator values, will be returned first. In the case of HOUGH_GRADIENT_ALT algorithm, this is the circle "perfectness" measure. The closer it to 1, the better shaped circles algorithm selects. In most cases 0.9 should be fine. If you want get better detection of small circles, you may decrease it to 0.85, 0.8 or even less. But then also try to limit the search range [minRadius, maxRadius] to avoid many false circles.
-  # minRadius	Minimum circle radius.
-  # maxRadius	Maximum circle radius. If <= 0, uses the maximum image dimension. If < 0, HOUGH_GRADIENT returns centers without finding the radius. HOUGH_GRADIENT_ALT always computes circle radiuses.
-
+  # Detect circles using Hough transform
   detected_circles_raw=cv2.HoughCircles(
     gimg, 
     cv2.HOUGH_GRADIENT, 
@@ -146,49 +92,38 @@ def count_cherries(img,iter_idx):
     minRadius=15, 
     maxRadius=40
   )
-
-  # params = cv2.SimpleBlobDetector_Params()
-  # detector = cv2.SimpleBlobDetector_create(params)
-  # detected_circles = detector.detect(gimg)
-
-
-  # Draw circles that are detected.  
   if detected_circles_raw is None:
     print("number detected circle = 0")
     return 0
-
-  # print(detected_circles_raw.shape)
   
+  # Filter : center of circle needs to be filled
   detected_circles = []
+  # kernel1 = np.array([[1, 1, 1],
+  #                     [1, 1, 1],
+  #                     [1, 1, 1]])
+  # # filter2D() function can be used to apply kernel to an image.
+  # # Where ddepth is the desired depth of final image. ddepth is -1 if...
+  # # ... depth is same as original or source image.
+  # fimg = cv2.filter2D(src=gimg, ddepth=-1, kernel=kernel1)
+  # cv2.imwrite("filtered/image{}_filtered.jpg".format(iter_idx), fimg)
   for pt in detected_circles_raw[0, :]:
-    value = gimg[int(pt[1]), int(pt[0])]
+    value = local_minimum(gimg, int(pt[1]), int(pt[0]))
     if value > 10:
-      # print("Detected new circle")
       detected_circles.append(pt)
   
-  # print(detected_circles)
-  
+  # Image with detected circles
   dimg = cv2.cvtColor(gimg,cv2.COLOR_GRAY2BGR)
 
-  # print(detected_circles)
-
+  # Draw circles that are detected.  
   for pt in detected_circles:
     pt = np.round(pt).astype(np.int32)
-    # print(pt)
     a, b, r = pt[0], pt[1], pt[2]
     cv2.circle(dimg, (a, b), r, (0, 255, 0), 2)
     cv2.circle(dimg, (a, b), 1, (0, 0, 255), 3)
-    #cv2.imshow("Detected Circle", frame) 
-
-  cv2.imwrite("detected/image{}_detected.jpg".format(iter_idx), dimg);
-  # # Convert the circle parameters a, b and r to integers.
-  # detected_circles= np.uint16(np.around(detected_circles))
-  # print(detected_circles)
-  # print (len(detected_circles[0, :]))
+  cv2.imwrite("detected/image{}_detected.jpg".format(iter_idx), dimg)
+  
   num_cherries = len(detected_circles)
-
-  return num_cherries;
-  # ~ return retval-1;
+  return num_cherries
 
 if __name__ == "__main__":
 
